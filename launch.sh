90125b3971e848df1c3303e0cc944891230af934
Batool Al-Rifai <balrifai@hawk.iit.edu>
2015-11-09 16:56:05 -0600

    added cleanup.sh, declared instance array, and implemented wait command for instances

--- launch.sh
+++ launch.sh
@@ -6,3 +6,14 @@
 declare -a instanceARR
 mapfile -t instanceARR < < (aws ec2 run-instances --image-id ami-d05e75b8 --count $1 --instance-type t2.micro --key-name itmo444-virtualbox --security-group-ids sg- --subnet-id subnet-
 --associate-public-address --iam-instance-profile Name=phpdeveloperRole --user-data file://itmo444-env/install-webserver.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
+#display array contents
+echo {instanceARR[@]}
+
+#wait till instances are launched
+aws ec2 wait instance-running --instance-ids ${instanceARR[@]}
+
+#tell user that instances are running
+echo "instances are running"
+@
+@
+
