304596ccb434e5b2cfae26e302b948adc1fdadc6
Batool Al-Rifai <balrifai@hawk.iit.edu>
2015-11-09 18:48:48 -0600

    commented out for testing, changed file path

--- launch.sh
+++ launch.sh
@@ -4,31 +4,36 @@
 
 #declare an array in bash
 declare -a instanceARR
-mapfile -t instanceARR < < (aws ec2 run-instances --image-id ami-d05e75b8 --count $1 --instance-type t2.micro --key-name itmo444-virtualbox --security-group-ids sg-5bba5c3d --subnet-id subnet-b92c7692
---associate-public-address --iam-instance-profile Name=phpdeveloperRole --user-data file://itmo444-env/install-webserver.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
+mapfile -t instanceARR < <(aws ec2 run-instances --image-id ami-d05e75b8 --count $1 --instance-type t2.micro --key-name itmo444-virtualbox --security-group-ids sg-5bba5c3d --subnet-id subnet-b92c7692 --associate-public-ip-address --iam-instance-profile Name=phpdeveloperRole --user-data file://install-webserver.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
 #display array contents
-echo {instanceARR[@]}
+echo ${instanceARR[@]}
 
 #wait till instances are launched
 aws ec2 wait instance-running --instance-ids ${instanceARR[@]}
 
 #tell user that instances are running
 echo "instances are running"
-@
-@
+
 
 #create load balancer array
-ELBURL=('aws elb create-load-balancer --load-balancer-name $2 --listeners Protocol=HTTP,LoadBalancerPort=80 --subnets subnet-b92c7692 --security-groups sg-5bba5c3d --output=text');
-echo $ELBURL
+#ELBURL=('aws elb create-load-balancer --load-balancer-name $2 --listeners Protocol=HTTP,LoadBalancerPort=80 --subnets subnet-b92c7692 --security-groups sg-5bba5c3d --output=text');
+#echo $ELBURL
 
-echo -e "\nELB has loaded, waiting 25 seconds"
-echo -e "\n"
-for i in {0..25};do echo -ne '.';sleep 1;done
-echo -e "\n"
+#echo -e "\nELB has loaded, waiting 25 seconds"
+#echo -e "\n"
+#for i in {0..25};do echo -ne '.';sleep 1;done
+#echo -e "\n"
 
 #attach/register lb to instances
-aws elb register-instances-with-load-balancer --load-balancer-name $2 --instances ${instanceARR[@]}
+#aws elb register-instances-with-load-balancer --load-balancer-name $2 --instances ${instanceARR[@]}
 
 #health check policy based on given parameters
-aws elb configure-health-check --load-balancer-name $2 --health-check Target=HTTP:80/index.html,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
- 
+#aws elb configure-health-check --load-balancer-name $2 --health-check Target=HTTP:80/index.html,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
+
+#create launch config to launch instances
+
+#create cloud watch metrics
+
+#create autoscaling group
+
+#create aws rds instance 
