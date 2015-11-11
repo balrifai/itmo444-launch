f8ce5183e54deeeca3efb303424bb82cd23593e8
Batool Al-Rifai <balrifai@hawk.iit.edu>
2015-11-10 19:32:20 -0600

    created cloud watch metrics

--- launch.sh
+++ launch.sh
@@ -30,9 +30,15 @@ aws elb register-instances-with-load-balancer --load-balancer-name $2 --instance
 aws elb configure-health-check --load-balancer-name $2 --health-check Target=HTTP:80/index.html,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
 
 #create launch config to launch instances
-aws autoscaling create-launch-configuration --launch-configuration-name balrifai-launch-config image-id ami-d05e75b8 --key-name itmo444-virtualbox --security-groups sg-5bba5c3d --instance-type t2.micro --user-data file://install-webserver.sh --iam-instance-profile Name=phpdeveloperRole 
+aws autoscaling create-launch-configuration --launch-configuration-name balrifai-launch-config --image-id ami-d05e75b8 --key-name itmo444-virtualbox --security-groups sg-5bba5c3d --instance-type t2.micro --user-data file://install-webserver.sh --iam-instance-profile Name=phpdeveloperRole 
 
 #create cloud watch metrics
+METRIC1=(`aws autoscaling put-scaling-policy --policy-name itmo444pol1 --auto-scaling-group-name balrifai-scaling --scaling-adjustment 1 --adjustment-type ChangeInCapacity`);
+METRIC2=(`aws autoscaling put-scaling-policy --policy-name itmo444pol2 --auto-scaling-group-name balrifai-scaling --scaling-adjustment 1 --adjustment-type ChangeInCapacity`);
+
+aws cloudwatch put-metric-alarm --alarm-name AddCapacity --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 120 --threshold 30 --comparison-operator GreaterThanOrEqualToThreshold --dimensions "Name=AutoScalingGroupName,Value=balrifai-scaling" --evaluation-periods 2 --alarm-actions $METRIC1
+aws cloudwatch put-metric-alarm --alarm-name RemoveCapacity --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 120 --threshold 10 --comparison-operator LessThanOrEqualToThreshold --dimensions "Name=AutoScalingGroupName,Value=balrifai-scaling" --evaluation-periods 2 --alarm-actions $METRIC2 
+
 
 #create autoscaling group
 
