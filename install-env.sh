0c36d9f5b3ccbbb4e7aba21e198dc22ca1d61ebf
Batool Al-Rifai <balrifai@hawk.iit.edu>
2015-11-10 23:45:25 -0600

    modified echo output

--- install-env.sh
+++ install-env.sh
@@ -7,4 +7,4 @@ git clone https://github.com/balrifai/itmo444-appsetup.git
 mv ./itmo444-appsetup/images /var/www/html/images
 mv ./itmo444-appsetup/index.html /var/www/html
 mv ./itmo444-appsetup/page2.html /var/www/html 
-echo "Hello!" > /tmp/hello.txt
+echo "Batool Al-Rifai" > /tmp/hello.txt
