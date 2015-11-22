70ccc8ed82d572600f843a52f668a5ee98848bad
Batool Al-Rifai <balrifai@hawk.iit.edu>
2015-11-22 00:30:10 -0600

    setup.php

--- install-webserver.sh
+++ install-webserver.sh
@@ -3,16 +3,24 @@
 sudo apt-get update -y
 sudo apt-get install -y php5 apache2 git php5-curl mysql-client curl php5-mysql
 
+cp -R vendor/ /var/www/html
+
 git clone https://github.com/balrifai/itmo444-appsetup.git
 
-mv ./itmo444-appsetup/images /var/www/html/images
-mv ./itmo444-appsetup/index.html /var/www/html
-mv ./itmo444-appsetup/*.php /var/www/html
+cp -R appsetup/* /var/www/html
+
+php /var/www/html/setup.php
+chmod 600 /var/www/html/setup.php
+
+mv ../itmo444-appsetup/images /var/www/html/images
+mv ../itmo444-appsetup/index.html /var/www/html
+mv ../itmo444-appsetup/*.php /var/www/html
 
 curl -s5 https://getcomposer.org/installer | php
 
 sudo php composer.phar require aws/aws-sdk.php
 
-sudo vendor /var/www/html &> /tmp/runcomposer.txt
+sudo mv  vendor /var/www/html &> /tmp/runcomposer.txt
+sudo php /var/www/html/setup.php &> /tmp/setup-db.txt
 
-echo "Hello" . /tmp/hello.txt 
+echo "Hello" > /tmp/hello.txt 
